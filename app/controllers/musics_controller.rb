9b12b578c7c4768c4ccfd7e0cdebb4146a56b28e
Aguru itabashi <itabashi.a@gmail.com>
2019-05-14 20:25:24 +0900

    music_helper実装完了

--- app/controllers/musics_controller.rb
+++ app/controllers/musics_controller.rb
@@ -8,29 +8,12 @@ class MusicsController < ApplicationController
   end
 
   def search
-    # youtube用
-    if params[:q] != ""
-      q = params[:q]
-      youtube = Google::Apis::YoutubeV3::YouTubeService.new
-      youtube.key = ENV['YOUTUBE_KEY']
-      youtube_response = youtube.list_searches("id,snippet", type: "video", q: q, max_results: 2)
-      @youtube_response = youtube_response.items
-    end
-
-    # mixcloud用
-    # && params[:mixcloud]
-    if params[:q] != ""
-      q = params[:q]
-      uri = URI.parse URI.encode("https://api.mixcloud.com/search/?q=#{q}&amp;type=cloudcast")
-      mixcloud_response = JSON.load(Net::HTTP.get(uri))
-      @mixcloud_response = mixcloud_response["data"]
-    end
+    youtube_api
+    mixcloud_api
 
     respond_to do |format|
         format.js{render :index}
     end
- 
-
   end
 
   def play
