0a31ef17c547a1b274ef1421a4969de5cb8c25d0
Aguru itabashi <itabashi.a@gmail.com>
2019-05-14 17:53:51 +0900

    Usersテーブルにカラムを追加

---
+++ db/migrate/20190514085126_add_about_oauth_column_to_users.rb
@@ -0,0 +1,11 @@
+class AddAboutOauthColumnToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :uid, :string, null:false, default:""
+    add_column :users, :provider, :string, null:false, default:""
+    add_column :users, :name, :string, null:false, default:""
+    add_column :users, :image_url, :string, null:false, default:""
+    add_index :users, [:uid, :provider], unique: true
+    add_column :users, :token, :string
+    add_column :users, :meta, :string
+  end
+end
