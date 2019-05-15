c47b51e1028e208dd8601f8021f8656dae98be49
Aguru itabashi <itabashi.a@gmail.com>
2019-05-15 21:52:57 +0900

    uid/providerカラムにdefault値を設定できていないことが判明

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,29 @@
+class Admin::UsersController < ApplicationController
+  def index
+    # N+1問題を後ほど考慮する
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(set_params)
+    if @user.save
+      flash[:success] = "ユーザー登録に成功しました"
+      redirect_to admin_users_path
+    else
+      flash[:danger] = "ユーザ登録に失敗しました"
+      render 'new'
+    end
+  end
+
+
+  private
+  
+  def set_params
+    params.require(:user).permit(:name,:email, :password, :password_confirmation, :admin)
+  end
+
+end
