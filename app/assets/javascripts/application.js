1eadba88cfed8d1ee88129ce8a5c02b6f8e393f0
Aguru itabashi <itabashi.a@gmail.com>
2019-05-16 17:52:47 +0900

    jqueryの記述を戻す

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,7 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
 //= require jquery/dist/jquery.js
 //= require bootstrap/dist/js/bootstrap.min
 //= require activestorage
