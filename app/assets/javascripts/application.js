99bb6b688e049d33bb13b3d37d11db6010cc84d6
Aguru itabashi <itabashi.a@gmail.com>
2019-05-16 17:48:57 +0900

    jqueryを削除

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,10 +11,8 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require jquery
 //= require jquery/dist/jquery.js
 //= require bootstrap/dist/js/bootstrap.min
 //= require activestorage
 //= require turbolinks
-//= jquery 
 //= require_tree .
