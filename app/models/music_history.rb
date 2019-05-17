0da7df133ecd9d387f26af60bb93667c1a091855
Aguru itabashi <itabashi.a@gmail.com>
2019-05-17 13:49:04 +0900

    Musicモデル/テーブルの生成

---
+++ app/models/music_history.rb
@@ -0,0 +1,4 @@
+class MusicHistory < ApplicationRecord
+  belongs_to :user
+  belongs_to :music
+end
---
+++ db/migrate/20190517044755_create_music_histories.rb
@@ -0,0 +1,10 @@
+class CreateMusicHistories < ActiveRecord::Migration[5.2]
+  def change
+    create_table :music_histories do |t|
+      t.references :user, foreign_key: true
+      t.references :music, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
