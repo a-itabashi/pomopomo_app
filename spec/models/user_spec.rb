fba0d02f2e0da918e98753fd594dde0de153704e
Aguru itabashi <itabashi.a@gmail.com>
2019-05-20 20:38:40 +0900

    アラートのCSS調整

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,12 +5,14 @@ RSpec.describe User, type: :model do
   describe "#name" do
     it "名前がなければ無効な状態かどうか" do
       user = User.new(name: nil)
-      expect(user).to be_invalid
+      user.valid?
+      expect(user.errors[:name]).to include("を入力してください")
     end
 
     it "名前が50文字を超えた場合、無効な状態かどうか" do
       user = User.new(name: "a"*51)
-      expect(user).to be_invalid
+      user.valid?
+      expect(user.errors[:name]).to include("は50文字以内で入力してください")
     end
   end
 
