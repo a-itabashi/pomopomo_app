ac8f361e1dccaf5a140b67d7ba9598d38e4bc6bc
Aguru itabashi <itabashi.a@gmail.com>
2019-05-29 15:14:49 +0900

    seedデータ生成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+20.times do |n|
+  name = Faker::Pokemon.name
+  email = Faker::Internet.email
+  uid = Faker::Internet.email
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password,
+               uid: uid,
+               )
+end
+
+1.times do |n|
+uid = Faker::Internet.email
+User.create!(name: "管理者ユーザー",
+             email: "admin@gmail.com",
+             password: "adminadmin",
+             password_confirmation: "adminadmin",
+             uid: uid,
+             admin: "true",
+            )
+end
+
+1.times do |n|
+uid = Faker::Internet.email
+User.create!(name: "testユーザー",
+             email: "test@gmail.com",
+             password: "testtest",
+             password_confirmation: "testtest",
+             uid: uid,
+            )
+end
+
+20.times do |n|
+  i = 1
+  content = Faker::Music.album
+  user_id = i
+  Post.create!(
+    content: content,
+    user_id: user_id,
+    )
+  i += 1
+end
